966c6e0e11ee32cc11f0fae257601f42503d573f
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-17 11:28:22 +0300

    LR1.N1 "Hello world"

---
+++ FS.fs
@@ -0,0 +1,29 @@
+﻿let circle_s radius =
+    System.Math.PI * radius * radius
+
+let cyl_vol_super (radius, height) = 
+    let cylinder_base_s = 
+        circle_s radius
+    height * cylinder_base_s
+
+let cyl_vol_car radius height =
+    let cylinder_base_s = 
+        circle_s radius
+    height * cylinder_base_s
+
+[<EntryPoint>]
+let main (args : string[]) =
+    printfn "Введите радиус и высоту:"
+    let radius = float (System.Console.ReadLine())
+    let height = float (System.Console.ReadLine())
+
+    let circle_s = circle_s radius
+    printfn "Площадь круга: %f" circle_s
+
+    let vol_super = cyl_vol_super (radius, height)
+    printfn "Объем цилиндра суперпоз: %f" vol_super
+    
+    let vol_car = cyl_vol_car radius height
+    printfn "Объем цилиндра кариров: %f" vol_car
+
+    0
