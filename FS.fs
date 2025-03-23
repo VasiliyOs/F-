0b1af67d3b5f34272cf46e0b512ad50dbd983443
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 17:54:59 +0300

    LR1.2 "Solve quadratic"

--- FS.fs
+++ FS.fs
@@ -1,2 +1,8 @@
-// For more information see https://aka.ms/fsharp-console-apps
- printfn "Hello World"
+printfn "Hello World"
+let solve_result (a, b, c) =
+    let D = b * b - 4.0 * a * c
+    ((-b + sqrt(D)) / (2.0 * a), (-b - sqrt(D)) / (2.0 * a));
+
+let roots = solve_result (1.0, 2.0, -3.0)
+
+printfn "Корни уравнения: %A" roots
