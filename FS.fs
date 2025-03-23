f1a697ee14588431c065a746aa16b2890efdfad5
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 18:12:44 +0300

    LR1.5 "Check result sum digits down"

--- FS.fs
+++ FS.fs
@@ -1,40 +1,11 @@
-let solve_result (a, b, c) =
-    let D = b * b - 4.0 * a * c
-    ((-b + sqrt(D)) / (2.0 * a), (-b - sqrt(D)) / (2.0 * a));
-    
-let circle_s radius =
-    System.Math.PI * radius * radius
-
-let cyl_vol_super (radius, height) = 
-    let cylinder_base_s = 
-        circle_s radius
-    height * cylinder_base_s
-
-let cyl_vol_car radius height =
-    let cylinder_base_s = 
-        circle_s radius
-    height * cylinder_base_s
-
 [<EntryPoint>]
-let main (args : string[]) =
-    let roots = solve_result (1.0, 2.0, -3.0)
-    
-    printfn "Введите радиус и высоту:"
-    let radius = float (System.Console.ReadLine())
-    let height = float (System.Console.ReadLine())
+     let main argv =
+         let number = 12345
+         
+         let result = sum_digits_down number
+         printfn "Сумма цифр равна %d" result
 
-    let circle_s = circle_s radius
-    printfn "Площадь круга: %f" circle_s
-
-    let vol_super = cyl_vol_super (radius, height)
-    printfn "Объем цилиндра суперпоз: %f" vol_super
-    
-    let vol_car = cyl_vol_car radius height
-    printfn "Объем цилиндра кариров: %f" vol_car
-    
-    let number = 12345
-    let result = WorkWithDigits.sum_digit_recursion_top number
-    printfn "Сумма цифр равна %d" result
-    
-    0
+         let result = sum_digits_top number
+         printfn "Сумма цифр равна %d" result
+         0;;
 
