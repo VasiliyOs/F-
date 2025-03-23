7bb90e19ccc695e3e9c3ac6ba5ed5fd0752b70c9
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 18:05:52 +0300

    LR1.4 "Add file WorkWithDigits"

---
+++ WorkWithDigits.fs
@@ -0,0 +1,8 @@
+﻿module WorkWithDigits
+    let sum_digit_recursion_top number = 
+        let rec sum_digit number cur_sum =
+            if number = 0 then cur_sum
+            else 
+                let last_digit = number % 10
+                sum_digit (number / 10) (cur_sum + last_digit)
+        sum_digit number 0
