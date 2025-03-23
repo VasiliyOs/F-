abc7846cfe17a2ea94928b39aa5605135f681f06
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 18:21:22 +0300

    LR1.6 "Check function"

--- FS.fs
+++ FS.fs
@@ -2,10 +2,13 @@
      let main argv =
          let number = 12345
          
-         let result = sum_digits_down number
+         let result = WorkWithDigits.sum_digits_down number
          printfn "Сумма цифр равна %d" result
 
-         let result = sum_digits_top number
+         let result = WorkWithDigits.sum_digits_top number
          printfn "Сумма цифр равна %d" result
-         0;;
 
+         let result = WorkWithDigits.big_function (false)
+         System.Console.WriteLine (result (6))
+
+         0
