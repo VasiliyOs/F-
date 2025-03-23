f4475bf4fa7b0d8b0bb07f02361db12561d1784c
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 18:32:22 +0300

    LR1.7 "Test function operation on digits of number"

--- FS.fs
+++ FS.fs
@@ -11,4 +11,9 @@
          let result = WorkWithDigits.big_function (false)
          System.Console.WriteLine (result (6))
 
+         let number = 123
+         let initialValue = 0
+         let result = WorkWithDigits.operation_on_digits_numbers number WorkWithDigits.plus initialValue
+         System.Console.WriteLine(result) 
+
          0
