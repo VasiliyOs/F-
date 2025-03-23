7c87b2d146f2ee1cd39a98c066d317a069a195af
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 18:11:25 +0300

    LR1.5 "Add sum digits recursion down"

--- WorkWithDigits.fs
+++ WorkWithDigits.fs
@@ -1,8 +1,16 @@
-﻿module WorkWithDigits
+module WorkWithDigits
     let sum_digit_recursion_top number = 
-        let rec sum_digit number cur_sum =
-            if number = 0 then cur_sum
-            else 
-                let last_digit = number % 10
-                sum_digit (number / 10) (cur_sum + last_digit)
-        sum_digit number 0
+    let rec sum_digit number cur_sum =
+        if number = 0 then cur_sum
+        else 
+            let last_digit = number % 10
+            sum_digit (number / 10) (cur_sum + last_digit)
+    sum_digit number 
+
+    let sum_digits_down number = 
+         let rec sum_digit number =
+             if number = 0 then 0
+             else 
+                 let last_digit = number % 10
+                 last_digit + sum_digit (number / 10)
+         sum_digit number 0
