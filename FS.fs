2a1da73eab78fdff0b5379cf7d5849a2066a221c
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 18:34:03 +0300

    LR1.8"Test function on sum, proiz, min, max"

--- FS.fs
+++ FS.fs
@@ -12,8 +12,22 @@
          System.Console.WriteLine (result (6))
 
          let number = 123
-         let initialValue = 0
-         let result = WorkWithDigits.operation_on_digits_numbers number WorkWithDigits.plus initialValue
-         System.Console.WriteLine(result) 
+
+         let sum_function = fun a b -> a + b
+         let proiz_function = fun a b -> a * b
+         let min_function = fun a b -> if a < b then a else b
+         let max_function = fun a b -> if a > b then a else b
+     
+         let sumResult = WorkWithDigits.operation_on_digits_numbers (number) (sum_function) (10)
+         System.Console.WriteLine("Сумма цифр числа: {0}", sumResult)
+     
+         let proizResult = WorkWithDigits.operation_on_digits_numbers (number) (proiz_function) (1)
+         System.Console.WriteLine("Произведение цифр числа: {0}", proizResult)
+     
+         let minResult = WorkWithDigits.operation_on_digits_numbers (number) (min_function) (9)
+         System.Console.WriteLine("Минимальная цифра числа: {0}", minResult)
+     
+         let maxResult = WorkWithDigits.operation_on_digits_numbers (number) (max_function) (0)
+         System.Console.WriteLine("Максимальная цифра числа: {0}", maxResult)
 
          0
