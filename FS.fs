933792f063b2204b6a661bcfc3dfc2ef0b1fbc8d
VasiliyOs <vasiliy2809_ne@mail.ru>
2025-03-23 17:57:37 +0300

    LR1.3 "Function S circle and V cilinder"

--- FS.fs
+++ FS.fs
@@ -1,8 +1,36 @@
-printfn "Hello World"
 let solve_result (a, b, c) =
     let D = b * b - 4.0 * a * c
     ((-b + sqrt(D)) / (2.0 * a), (-b - sqrt(D)) / (2.0 * a));
+    
+let circle_s radius =
+    System.Math.PI * radius * radius
 
-let roots = solve_result (1.0, 2.0, -3.0)
+let cyl_vol_super (radius, height) = 
+    let cylinder_base_s = 
+        circle_s radius
+    height * cylinder_base_s
 
-printfn "Корни уравнения: %A" roots
+let cyl_vol_car radius height =
+    let cylinder_base_s = 
+        circle_s radius
+    height * cylinder_base_s
+
+[<EntryPoint>]
+let main (args : string[]) =
+    let roots = solve_result (1.0, 2.0, -3.0)
+    
+    printfn "Введите радиус и высоту:"
+    let radius = float (System.Console.ReadLine())
+    let height = float (System.Console.ReadLine())
+
+    let circle_s = circle_s radius
+    printfn "Площадь круга: %f" circle_s
+
+    let vol_super = cyl_vol_super (radius, height)
+    printfn "Объем цилиндра суперпоз: %f" vol_super
+    
+    let vol_car = cyl_vol_car radius height
+    printfn "Объем цилиндра кариров: %f" vol_car
+
+    0
+
